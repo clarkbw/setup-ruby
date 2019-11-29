bec55060e6308670cd77590767c2fd3b3106766b
Bryan Clark <clarkbw@github.com>
2019-11-29 10:57:16 -0800

    format bundler, uppercase env

--- src/auth/bundler.ts
+++ src/auth/bundler.ts
@@ -3,14 +3,14 @@ import * as core from '@actions/core';
 
 // only exported for tests
 export function generateBundlerEnv(host: string) {
-    const url = new URL(host);
-    const domain = url.hostname;
-    return `BUNDLE_${domain.replace('.', '__')}`;
+  const url = new URL(host);
+  const domain = url.hostname.toUpperCase();
+  return `BUNDLE_${domain.replace(/\./g, '__')}`;
 }
 
 export default function(host: string, username: string, password: string) {
-    // env['BUNDLE_RUBYGEMS__PKG__GITHUB__COM'] = USERNAME:PASSWORD
-    const env = generateBundlerEnv(host);
-    console.log(`exporting bundler env ${env}`);
-    core.exportVariable(env, `${username}:${password}`);
+  // env['BUNDLE_RUBYGEMS__PKG__GITHUB__COM'] = USERNAME:PASSWORD
+  const env = generateBundlerEnv(host);
+  console.log(`exporting bundler env ${env}`);
+  core.exportVariable(env, `${username}:${password}`);
 }
