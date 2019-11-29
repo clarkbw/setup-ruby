d63b5d67d94f438ff67ec591fc7d84a4a3dff2e2
Bryan Clark <clarkbw@github.com>
2019-11-29 10:57:43 -0800

    move away from actions/io

--- src/auth/gem.ts
+++ src/auth/gem.ts
@@ -1,29 +1,26 @@
-import * as fs from 'fs';
+import {promises as fs} from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import * as core from '@actions/core';
-import * as io from '@actions/io';
 
 export const GEM_DIR = '.gem';
 export const CREDENTIALS_FILE = 'credentials';
 
-export default async function(username: string, password: string) {
-    const directory: string = path.join(os.homedir(), GEM_DIR);
-    await io.mkdirP(directory);
-    core.debug(`created directory ${directory}`);
-    await write(directory, generate(username, password));
+export default async function(key: string, password: string) {
+  const directory: string = path.join(os.homedir(), GEM_DIR);
+  await fs.mkdir(directory, {recursive: true});
+  core.debug(`created directory ${directory}`);
+  await write(directory, generate(key, password));
 }
 
 // only exported for testing purposes
-// The username becomes the key to access this token via the `gem` command
-// Example: gem push --key ${username} *.gem
-export function generate(username: string, password: string) {
-    return `---\n:${username}: Bearer ${password}\n`;
+export function generate(key: string, password: string) {
+  return `---\n:${key}: Bearer ${password}\n`;
 }
 
 async function write(directory: string, credentials: string) {
-    const options = {encoding: 'utf-8'};
-    const location = path.join(directory, CREDENTIALS_FILE);
-    console.log(`writing ${location}`);
-    return fs.writeFileSync(location, credentials, options);
+  const options = {encoding: 'utf-8'};
+  const location = path.join(directory, CREDENTIALS_FILE);
+  console.log(`writing ${location}`);
+  return fs.writeFile(location, credentials, options);
 }
