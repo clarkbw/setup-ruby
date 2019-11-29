9fd176f469793b8af47d843130dad6da986830c0
Bryan Clark <clarkbw@github.com>
2019-11-29 10:58:20 -0800

    Use param object

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,20 +2,29 @@ import * as core from '@actions/core';
 import bundler from './bundler';
 import gem from './gem';
 
-// This supports 2 authentication methods for bundler and gem as they are the 
+interface GemBundlerParameters {
+  readonly key?: string;
+  readonly host?: string;
+  readonly username?: string;
+  readonly password: string;
+}
+
+// This supports 2 authentication methods for bundler and gem as they are the
 // most common tools used for Ruby Gems
 // Supplying a registry host, username and password supports both bundler and gem
 // Not suppying a host name doesn't allow for bundler support
-export async function configAuthentication(host: string, username: string, password: string) {
-  if (host && username && password) {
-    console.log(`configure bundler and gem support ${username}@${host} and password`);
-    await bundler(host, username, password);
-    await gem(username, password);
-  } else if (username && password) {
-    await gem(username, password);
+export async function configAuthentication(config: GemBundlerParameters) {
+  if (config.host && config.username) {
+    console.log(
+      `configuring bundler for ${config.host} = ${config.username}: a password`
+    );
+    await bundler(config.host, config.username, config.password);
+  } else if (config.key) {
+    console.log(`configuring gem for ${config.key} and a password`);
+    await gem(config.key, config.password);
   } else {
     core.debug(
-      `no auth without username: ${username} and password: ${password}`
+      `neither bundler nor gem configured without ${config.host} nor ${config.key}`
     );
   }
 }
