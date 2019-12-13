d19271ad416e845ce85941623f1dcfc6ce2feb12
Bryan Clark <clarkbw@github.com>
2019-12-13 11:38:40 -0800

    Fixing bundler exports

--- src/auth/bundler.ts
+++ src/auth/bundler.ts
@@ -1,5 +1,5 @@
-// import * as url from 'url';
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 // only exported for tests
 export function generateBundlerEnv(host: string) {
@@ -8,9 +8,15 @@ export function generateBundlerEnv(host: string) {
   return `BUNDLE_${domain.replace(/\./g, '__')}`;
 }
 
-export default function(host: string, username: string, password: string) {
+export default async function(
+  host: string,
+  username: string,
+  password: string
+) {
   // env['BUNDLE_RUBYGEMS__PKG__GITHUB__COM'] = USERNAME:PASSWORD
   const env = generateBundlerEnv(host);
   console.log(`exporting bundler env ${env}`);
   core.exportVariable(env, `${username}:${password}`);
+  // core.exportVariable('BUNDLE_GEM__PUSH_KEY', password);
+  core.exportVariable('RUBYGEMS_HOST', host);
 }
